eedec998d773a4a977e121a48d1124d37035008e
RafaelMedeirosDev <oliver.rafadev@gmail.com>
2025-09-14 00:52:05 -0300

    initial commit with Spring Boot project setup (Subscriptions Management)

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS users (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS billings (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    plataforma_name VARCHAR(100) NOT NULL,
+    user_id uuid REFERENCES users(id),
+    value DECIMAL(10, 2) NOT NULL,
+    billing_date DATE NOT NULL
+);
